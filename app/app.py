5749fc03a21c10e12c588fc5e905e5af9ca0b902
Kaique Alencar <kaique.alencar@gmail.com>
2025-11-25 11:55:40 -0300

    corrigindo login

--- app/app.py
+++ app/app.py
@@ -4,26 +4,45 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "86de935e75aaf84e5199eacc2008ab2cbb32b362df82654d5c9c8a0317c81715"  
+USUARIO = "admin"
+SENHA = "1234"
+
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        usuario = request.form.get('usuario')
+        senha = request.form.get('senha')
+
+        if usuario == USUARIO and senha == SENHA:
+            return redirect(url_for('index'))
+        else:
+            erro = "Usuário ou senha incorretos"
+
+    return render_template('login.html', erro=erro)
+""""
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         usuario = request.form.get("usuario")
         senha = request.form.get("senha")
 
-        if usuario == "admin" and senha == "admin123456789":
+        if usuario == "admin" and senha == "admin1234":
             session["usuario"] = usuario
             return redirect(url_for("home"))
-        else:
-            return render_template("login.html", erro="Usuário ou senha incorretos!")
+
+        return render_template("login.html", erro="Usuário ou senha incorretos!")
 
     return render_template("login.html")
-
-
+"""
 @app.route("/logout")
 def logout():
-    session.pop("usuario", None)
+    session.clear("usuario", None)
     return redirect(url_for("login"))
 
 def login_requerido(func):
@@ -38,6 +57,9 @@ def login_requerido(func):
 @app.route("/", methods=["GET"])
 @login_requerido
 def home():
+    if "usuario" not in session:
+        return redirect(url_for("login"))
+
     return render_template("index.html")
 
 
@@ -57,6 +79,8 @@ def gerar_pdf():
         as_attachment=True,
         download_name=os.path.basename(caminho_pdf)
     )
+
+
 if __name__ == "__main__":
     if not os.environ.get("RENDER"):
         app.run(debug=True)
