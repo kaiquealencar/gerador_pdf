3516b6c345a83d22fcdf1b7b31dc6769bc71efbf
Kaique Alencar <kaique.alencar@gmail.com>
2025-12-02 10:06:48 -0300

    correção mês de janeiro

--- app/periodo_mes.py
+++ app/periodo_mes.py
@@ -1,14 +1,18 @@
 from datetime import date
 import calendar
-
 hoje = date.today()
 
 
 
 
 def periodo_mes(mes_referencia: int):
-    primeiro_dia = date(date.today().year, mes_referencia -1, 1)
-    ultimo_dia = date(date.today().year, mes_referencia -1, calendar.monthrange(date.today().year, mes_referencia -1)[1]) 
+    
+    if mes_referencia == 1:
+        primeiro_dia = date(date.today().year, 12, 1)
+        ultimo_dia = date(date.today().year, 12, calendar.monthrange(date.today().year, 12)[1])        
+    else:
+        primeiro_dia = date(date.today().year, mes_referencia -1, 1)
+        ultimo_dia = date(date.today().year, mes_referencia -1, calendar.monthrange(date.today().year, mes_referencia -1)[1]) 
     
     return f"{ primeiro_dia.strftime('%d/%m/%Y') } A { ultimo_dia.strftime('%d/%m/%Y') }"
 
